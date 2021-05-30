bccc8e4f28e40bb779af4a1974c4adefb460baa6
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-05-30 22:31:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BankApplication
+ Программирование на языке высокого уровня. 6 семестр. Проект BankApplication
---
+++ src/main/java/org/bankapplication/App.java
@@ -0,0 +1,34 @@
+package org.bankapplication;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.net.URL;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader loginloader = new FXMLLoader();
+        URL loginUrl = getClass().getClassLoader().getResource("loginscene.fxml");
+        loginloader.setLocation(loginUrl);
+        Parent root = loginloader.load();
+        stage.getIcons().add(new Image("/icon.png"));
+        stage.setTitle("Bank Application");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
